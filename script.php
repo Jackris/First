242d6d69c327e0521bff1ee925105d95650416f8
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-17 20:01:03 +0300

    Update script.php

--- script.php
+++ script.php
@@ -13,9 +13,7 @@ function CBR_XML_Yest_Ru() {
     $json_yest_file = __DIR__.'/yest.json';
     $json_daily = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp?date_req=14/04/2020');
     file_put_contents($json_yest_file, $json_daily);
-        if ($json_daily = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js')) {
-            file_put_contents($json_yest_file, $json_daily);
-        }
+
     return json_decode(file_get_contents($json_yest_file));
 }
 $data = CBR_XML_Daily_Ru();
