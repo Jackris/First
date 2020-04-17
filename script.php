8ee768adbb6d2c540b2e0c69f9732f66bae6bd38
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-17 16:31:24 +0300

    Create script.php

---
+++ script.php
@@ -0,0 +1,15 @@
+<?php
+define("BASE_URL", "http://www.cbr.ru/scripts/");
+
+function api_request($resource, $Date) {
+    
+    $full_url = BASE_URL."$resource";
+    $options = array(
+        DATE_REQ=> $Date
+   );
+}
+$Date=16/04/2020;
+$res = api_request("XML_daily.asp?","16/04/2020");
+   Echo $res;
+
+?>
