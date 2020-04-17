e960c0099a70cf32ee3608e94ee00326dee975ff
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-17 19:52:42 +0300

    Update script.php

--- script.php
+++ script.php
@@ -9,10 +9,17 @@ function CBR_XML_Daily_Ru() {
     }
     return json_decode(file_get_contents($json_daily_file));
 }
-
+function CBR_XML_Yest_Ru() {
+    $json_yest_file = __DIR__.'/yest.json';
+    $json_daily = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp?date_req=16/04/2020');
+    file_put_contents($json_yest_file, $json_daily);
+        if ($json_daily = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js')) {
+            file_put_contents($json_yest_file, $json_daily);
+        }
+    return json_decode(file_get_contents($json_yest_file));
+}
 $data = CBR_XML_Daily_Ru();
-$json_daily = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp?date_req=16/04/2020');
-$final = json_decode(file_get_contents($json_daily));
+$final = CBR_XML_Yest_Ru();
 echo "Обменный Курс USD на ВЧЕРА: {$final->Valute->USD->Value} \n";
 echo "Обменный курс USD по ЦБ РФ на сегодня: {$data->Valute->USD->Value} \n";
 echo "Обменный курс EURO по ЦБ РФ на сегодня: {$data->Valute->EUR->Value} \n";
