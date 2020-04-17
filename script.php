d51c7d823b4842272d8706eaddaa033859a353c8
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-17 19:28:37 +0300

    Update script.php

--- script.php
+++ script.php
@@ -7,11 +7,12 @@ function CBR_XML_Daily_Ru() {
             file_put_contents($json_daily_file, $json_daily);
         }
     }
-
     return json_decode(file_get_contents($json_daily_file));
 }
 
 $data = CBR_XML_Daily_Ru();
 
 echo "Обменный курс USD по ЦБ РФ на сегодня: {$data->Valute->USD->Value}";
+echo "Обменный курс EURO по ЦБ РФ на сегодня: {$data->Valute->EURO->Value}";
+echo " ";
 ?>
