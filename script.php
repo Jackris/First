5c200eb00faafaea2a69a1a4e11e2d183ec8a091
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-18 17:02:09 +0300

    Update script.php

--- script.php
+++ script.php
@@ -3,6 +3,7 @@ $date = "22/01/2007";
 $xml = simplexml_load_file('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req={$date}');
 Echo $xml->Valute[4]->Value;
 echo "\n";
+echo $date;
 //print_r($xml->ValCurs->Valute[ID =="R01235"]);
  //'▲'
 //'▼'
