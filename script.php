7fb0be558b8b34e3451fd850f9ce07475f2553bd
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-18 17:40:36 +0300

    Update script.php

--- script.php
+++ script.php
@@ -4,7 +4,10 @@ function getvalute($date)
     $xml = simplexml_load_file('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req='. $date);
     $valueUSD = $xml->Valute[4]->Value; // Значение доллара
     $valuseEUR = $xml->Valute[5]->Value; // Значение евро
-    $ndate = strtotime('-3 days');
+    $ndate = new DateTime('-3 days');
+    echo $ndate->format('Y-m-d');
+    //$ndate = strtotime('-3 days');
+    //echo "USD = ".$valueUSD ."\n";
     echo date('Y-m-d', $date);
 }
 getvalute("01/02/2007");
