69039fb466d3c69bc0ef68a417fb4ffc9a5ca0b0
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-18 17:01:38 +0300

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-$date = 22/01/2007;
+$date = "22/01/2007";
 $xml = simplexml_load_file('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req={$date}');
 Echo $xml->Valute[4]->Value;
 echo "\n";
