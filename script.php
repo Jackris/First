503854415fe73a8f4ed3bbe93b68e0a7d4e9fd4d
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-18 17:08:54 +0300

    Update script.php

--- script.php
+++ script.php
@@ -3,8 +3,8 @@ $date = "22/01/2007";
 $xml = simplexml_load_file('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req='. $date);
 $valueUSD = $xml->Valute[4]->Value;
 $valuseEUR = $xml->Valute[5]->Value;
-echo $valueUSD;
-echo $valuseEUR;
+echo "USD = ".$valueUSD .\n;
+echo "EURO = ".$valuseEUR .\n;
 echo "\n";
 //print_r($xml->ValCurs->Valute[ID =="R01235"]);
  //'▲'
