8d6dba53a6e512b3fe8cc3d6bf0a3dc1fe910787
Jackris <63738847+Jackris@users.noreply.github.com>
2020-04-18 18:14:26 +0300

    Update script.php

--- script.php
+++ script.php
@@ -15,7 +15,7 @@ $ndate = $datetime->format('d-m-Y');
           
     echo "USD = ".$valueUSD ."\n";
 }
-getvalute("05/12/2007");
+getvalute("29/12/2007");
 /*$date = "22/01/2007";
 $xml = simplexml_load_file('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req='. $date);
 $valueUSD = $xml->Valute[4]->Value;
